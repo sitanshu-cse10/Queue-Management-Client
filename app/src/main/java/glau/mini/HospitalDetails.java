fbbbb6b21f91cdc3db609a9b730390f8acd71496
Mohil <mohilagrawal45@gmail.com>
2019-03-02 00:57:12 +0530

    3 pages(design)

---
+++ app/src/main/java/glau/mini/HospitalDetails.java
@@ -0,0 +1,13 @@
+package glau.mini;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HospitalDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_details);
+    }
+}
---
+++ app/src/main/java/glau/mini/HospitalDetails1.java
@@ -0,0 +1,23 @@
+package glau.mini;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class HospitalDetails1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_details1);
+    }
+}
