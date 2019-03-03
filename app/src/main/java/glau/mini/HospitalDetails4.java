1bd0c837234075725d52fddc85a9e59d51e6a63e
Vani Agrawal <shivanisinghmomo@gmail.com>
2019-03-03 13:41:48 +0530

    design(5)

---
+++ app/src/main/java/glau/mini/HospitalDetails4.java
@@ -0,0 +1,13 @@
+package glau.mini;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HospitalDetails4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_details4);
+    }
+}
